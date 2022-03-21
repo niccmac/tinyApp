7de247df7d19ab605e1fcb555247c91900ebc152
Nicole MacLean <nicole.mac0404@gmail.com>
2022-03-21 16:27:46 -0700

    added url/short url link page with fake create new url href

--- expressServer.js
+++ expressServer.js
@@ -1,32 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 8080; // Default port 8080
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 const urlDatabase = {
-  'b2xVn2': 'http://www.lighthouselabs.ca',
-  '9sm5xK': 'http://www.google.com'
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
 };
 
-app.get('/', (req, res) => {
-  res.send('Hello!');
+app.get("/", (req, res) => {
+  res.send("Hello!");
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get('/urls.json', (req, res) => {
+app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></htmml>\n');
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></htmml>\n");
 });
 
-app.get('/urls', (req, res) => {
+app.get("/urls", (req, res) => {
   const templateVars = {
     urls: urlDatabase
   };
-  res.render('urlsIndex', templateVars);
+  res.render("urlsIndex", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urlsShow", templateVars);
 });
