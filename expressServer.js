5021802d4ea55f082ff14f50c1272032ecc90fd2
Nicole MacLean <nicole.mac0404@gmail.com>
2022-03-21 18:45:33 -0700

    added form on url page

--- expressServer.js
+++ expressServer.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // Default port 8080
+const bodyParser = require("body-parser"); //Makes buffer data human readable - middleware.
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.set("view engine", "ejs");
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+app.post("/urls", (req, res) => {
+  console.log("req.body:", req.body);
+  res.send("OK");// This will be replaced later.
+});
+app.get('/urls/new', (req, res) => {
+  res.render("urlsNew");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -32,9 +42,6 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urlsShow", templateVars);
-  
- 
 });
