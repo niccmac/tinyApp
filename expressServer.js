b02b12194c3395efac91bfc9ff1ed5a5f585e5c5
Nicole MacLean <nicole.mac0404@gmail.com>
2022-03-23 08:11:07 -0700

    added LHL user database

--- expressServer.js
+++ expressServer.js
@@ -22,6 +22,22 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//
+//USER DATABASE
+//
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
 
 //
 // ROUTES
